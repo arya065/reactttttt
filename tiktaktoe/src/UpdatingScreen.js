b3cd3b9aaa689867363cc1f341585d5e4a82a8b3
arya065 <arya065@g.educaand.es>
2023-12-01 10:12:27 +0100

    ini

--- tiktaktoe/src/UpdatingScreen.js
+++ tiktaktoe/src/UpdatingScreen.js
@@ -1,4 +1,21 @@
+//сохраняем инфу при перезагрузке страницы https://react.dev/learn#updating-the-screen
+//ипортируем useState
+import { useState } from "react";
+
+//основной компонент, в который могу пихать сколько угодно чего угодно, но в одном теге
 function go() {
-  return <div>test</div>;
+  return (
+    <div>
+      <Mabutton />
+    </div>
+  );
+}
+//компонент-кнопка со счётчиком
+function Mabutton() {
+  function countNext() {
+    setcount(count + 1);
+  }
+  const [count, setcount] = useState(0);
+  return <button onClick={countNext}>Clicked here {count} times</button>;
 }
 export default go;
